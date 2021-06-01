d1cc6ebceedc1cc5acb8a5b8246b345dabcf045b
zhoubing <1050015785@qq.com>
2021-06-02 00:58:52 +0800

    吐槽模块

---
+++ tensquare_spit/src/main/java/com/tensquare/SpitApplication.java
@@ -0,0 +1,22 @@
+package com.tensquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * @author zhoubing
+ * @date 2021-06-02 00:26
+ */
+@SpringBootApplication
+public class SpitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpitApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker();
+    }
+}
---
+++ tensquare_spit/src/main/java/com/tensquare/spit/service/SpitService.java
@@ -0,0 +1,50 @@
+package com.tensquare.spit.service;
+
+import com.tensquare.spit.dao.SpitDao;
+import com.tensquare.spit.pojo.Spit;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import util.IdWorker;
+
+/**
+ * @author zhoubing
+ * @date 2021-06-02 00:31
+ */
+@Service
+public class SpitService {
+    @Autowired
+    private SpitDao spitDao;
+
+    @Autowired
+    private IdWorker idWorker;
+
+    public List<Spit> findAll() {
+        return spitDao.findAll();
+    }
+
+    public Spit findById(String id) {
+        return spitDao.findById(id).get();
+    }
+
+    public void add(Spit spit) {
+        spit.set_id(idWorker.nextId() + "");
+        spitDao.save(spit);
+    }
+
+    public void update(Spit spit) {
+        spitDao.save(spit);
+    }
+
+    public void deleteById(String id) {
+        spitDao.deleteById(id);
+    }
+
+    public Page<Spit> findByParentid(String parentid, int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
+        return spitDao.findByParentid(parentid, pageRequest);
+    }
+
+}
